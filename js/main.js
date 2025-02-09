ea41c1ef0ab18184029f4768626682cbe277c3af
mdsabuj6258 <mdsabuj1925@gmail.com>
2025-02-10 00:42:57 +0600

    add many new page

--- js/main.js
+++ js/main.js
@@ -85,6 +85,46 @@ $(function () {
     });
 
 
+    //======top munth cetagor SLIDER====== 
+    $('.POpular_post_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: false,
+        nextArrow: '<i class="far fa-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-arrow-left prevArrow"></i>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+
+
 
     //======top munth cetagor SLIDER====== 
     $('.top_munth_slider').slick({
